9ffaf14a8636efefcc9fc2648220e3f6711e535f
aroaroka111 <70784420+aroaroka111@users.noreply.github.com>
2020-09-25 23:48:08 +0300

    Update recent.js

--- commands/guardian/recent.js
+++ commands/guardian/recent.js
@@ -21,7 +21,7 @@ class RecentCommand extends Command {
             .setColor("#000000")
             .setTitle(`Recent Actions in ${message.guild.name}`)
             .setDescription(`*You can do **\`${message.guild.prefix}recent <ID>\`** to view all actions relating to a user.*`)
-            .setImage("https://cdn.discordapp.com/attachments/700409195719032921/758857832962457660/image0.gif")
+            .setImage("https://cdn.discordapp.com/avatars/758724364987793418/578e1d5e60c4091863344855a84da9a0.png?size=1024")
             .setFooter("BY DARK PLAN   BY cg ARO");
 
         let actions = message.guild.getActions(args.ID ? (i => i.executor.id === args.ID || (i.target && (i.target.id === args.ID))) : undefined);
