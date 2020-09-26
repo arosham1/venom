8a0d96ef5c5ad4407db282dcecc245c2bb149f8d
aroaroka111 <70784420+aroaroka111@users.noreply.github.com>
2020-09-27 00:36:36 +0300

    Update help.js

--- commands/guardian/help.js
+++ commands/guardian/help.js
@@ -23,11 +23,11 @@ class HelpCommand extends Command {
       embed.addField("__+/antibot off__","anti")
       embed.addField("__Owner bot__","<@619170695267614720>")
      embed.addField("__Co owner bot__","<@621497043663323149>")
-  ///   embed.addField("__Admin bot__","<@567826314334306307>")
+   embed.addField("__Admin bot__","<@567826314334306307>")
    //   embed.addField("__Removeperms__","Take All Permissions")
- //     embed.addField("__Support server __","https://discord.gg/TnPZzUF")
+   embed.addField("__Support server __","https://discord.gg/TnPZzUF")
    //     embed.addField("__Recover__", "Shows Recovery Menu")
-      const info = [`**[Direct Invite Link](https://discord.com/api/oauth2/authorize?client_id=758724364987793418&permissions=8&scope=bot)**`];
+      const info = [`**[link Bot server ](https://discord.com/api/oauth2/authorize?client_id=758724364987793418&permissions=8&scope=bot)**`];
       embed.addField('__Invite__', info.join('\n'));
       embed.setAuthor("Anti-Nuke Plugin Commands", "")
      // embed.setImage("https://cdn.discordapp.com/attachments/717616425040347166/717625089683881994/0e0a866e9011385fb8039376210467a6adc329ede0da13a0c15da6f3340020b9.jpeg")
