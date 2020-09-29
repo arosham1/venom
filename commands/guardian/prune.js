49c0f522f4e58066bb9dbb03f06b81469ad924b6
aroaroka111 <70784420+aroaroka111@users.noreply.github.com>
2020-09-29 06:19:15 +0300

    Update prune.js

--- commands/guardian/prune.js
+++ commands/guardian/prune.js
@@ -1,32 +1,4 @@
 
-const { Command } = require('discord-akairo');
-const { limits, adminCanChangeLimits } = require('../../config.js');
-
-class LimitsCommand extends Command {
-    constructor() {
-        super('prune', {
-            aliases: ['prune', 'prune'],
-            args: [
-                {
-                    id: 'index',
-                    type: 'integer'
-                },
-                {
-                    id: 'value',
-                    type: 'integer'
-                }
-            ],
-            channel: 'guild'
-        });
-    }
-
-
-
-
-
-
-
-
 
 ports.run = async function (bot, message, args) {
   message.reply("This will kick inactive people. Are you sure? Type `yes` for yes. Command will be canceled over 30 seconds.").then(() => {
@@ -56,5 +28,5 @@ exports.config = {
   enabled: true,
   guildOnly: true,
   permlevel: 6,
-  aliases: []
+  aliases: [prune]
 }
