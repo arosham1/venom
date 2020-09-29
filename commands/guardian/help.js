cea31dba0f1642d2fe41bc248f36c3b15d101b8a
aroaroka111 <70784420+aroaroka111@users.noreply.github.com>
2020-09-29 03:45:23 +0300

    Update help.js

--- commands/guardian/help.js
+++ commands/guardian/help.js
@@ -16,18 +16,18 @@ class HelpCommand extends Command {
 
 
       embed.setColor("#000000")
-      embed.addField("__+/anti__","Configure Your Server Limits `**OWNER ONLY**`")
-      embed.addField("__+/recent__","Shows Recent Actions")
-       embed.addField("__everyone dlete and here__","Reset Anti-Nuke Settings")
-      embed.addField("__+/antibot on__","anti")
-      embed.addField("__+/antibot off__","anti")
+      embed.addField("__+/anti__"," +/anti {number} `**Owner ship and admin strator**`")
+      embed.addField("__+/recent__","Shows Recent server")
+       embed.addField("__everyone dlete and here__","preimshon role MANGE MSAGEE")
+      embed.addField("__+/antibot on__","anti bot oline")
+      embed.addField("__+/antibot off__","anti ofline")
       embed.addField("__Owner bot__","<@619170695267614720>")
      embed.addField("__Co owner bot__","<@621497043663323149>")
-   embed.addField("__Admin bot__","<@567826314334306307>")
+   /// embed.addField("__Admin bot__","<@567826314334306307>")
    //   embed.addField("__Removeperms__","Take All Permissions")
    embed.addField("__Support server __","https://discord.gg/TnPZzUF")
    //     embed.addField("__Recover__", "Shows Recovery Menu")
-      const info = [`**[link Bot server ](https://discord.com/api/oauth2/authorize?client_id=758724364987793418&permissions=8&scope=bot)**`];
+      const info = [`**[AD LINK BOT SERVER ](https://discord.com/api/oauth2/authorize?client_id=758724364987793418&permissions=8&scope=bot)**`];
       embed.addField('__Invite__', info.join('\n'));
       embed.setAuthor("Anti-Nuke Plugin Commands", "")
      // embed.setImage("https://cdn.discordapp.com/attachments/717616425040347166/717625089683881994/0e0a866e9011385fb8039376210467a6adc329ede0da13a0c15da6f3340020b9.jpeg")
