df6a4a453e59451cfbca8e0bc8bc95eb6afdbe70
aroaroka111 <70784420+aroaroka111@users.noreply.github.com>
2020-11-24 22:43:39 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 //paste your token in the .env
 
 module.exports = {
-  token:   "NzU4NzI0MzY0OTg3NzkzNDE4.X2zHIQ.SI71IyPdxLODrn9nOwbS2X-j39A",
+  token:   "NzU4NzI0MzY0OTg3NzkzNDE4.X2zHIQ.8_0mBysPcpBu4pm72bzK5vv-Btg",
   defaultPrefix: "+/",
   ownerID: "619170695267614720",
   _limits: "The following are defaults.",
