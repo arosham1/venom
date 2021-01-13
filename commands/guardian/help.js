1d5b2f046a6d626e22dd92769b78238d39bc9678
arosham1 <70784420+arosham1@users.noreply.github.com>
2021-01-13 16:33:13 +0300

    Update help.js

--- commands/guardian/help.js
+++ commands/guardian/help.js
@@ -22,19 +22,19 @@ class HelpCommand extends Command {
       embed.addField("__+/antibot ob__","anti")
       embed.addField("__+/antibot off__","anti")
       embed.addField("__Owner bot__","<@619170695267614720>")
-     embed.addField("__Co owner bot__","<@621497043663323149>")
-     embed.addField("__BOT SECURIY SERVER DARK PLAN__","Automatically Recover (Unstable)")
+    // embed.addField("__Co owner bot__","<@621497043663323149>")
+    /// embed.addField("__BOT SECURIY SERVER DARK PLAN__","Automatically Recover (Unstable)")
    //   embed.addField("__Removeperms__","Take All Permissions")
  //     embed.addField("__Adminrestore__","Restore All Permissions")
    //     embed.addField("__Recover__", "Shows Recovery Menu")
       const info = [`**[Direct Invite Link](https://discord.com/api/oauth2/authorize?client_id=758724364987793418&permissions=8&scope=bot)**`];
       embed.addField('__Invite__', info.join('\n'));
-      embed.setAuthor("Anti-Nuke Plugin Commands", "")
+      embed.setAuthor("+/help", "")
      // embed.setImage("https://cdn.discordapp.com/attachments/717616425040347166/717625089683881994/0e0a866e9011385fb8039376210467a6adc329ede0da13a0c15da6f3340020b9.jpeg")
       embed.setThumbnail("")
       embed.setImage("https://cdn.discordapp.com/attachments/700409195719032921/758857832962457660/image0.gif")
     //  embed.setFooter("tno tyrell#5555")
-      embed.setFooter("BY DARK PLAN   BY Cg ARO")
+      embed.setFooter("HUNTER MERAMAR")
 
         message.channel.send(embed);
 
